83bd0d53ca8a27ca905ce4f66fe771e54a4b825d
Mahmoud <mymafm0405@gmail.com>
2020-12-13 19:12:17 +0300

    Now I can add dynamicly a form control

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  genders = ['male', 'female'];
+  signUpForm: FormGroup;
+
+  ngOnInit() {
+    this.signUpForm = new FormGroup({
+      'username': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([])
+    })
+  }
+
+  onSubmit() {
+    console.log(this.signUpForm);
+  }
+
+  getHobbies() {
+    return (<FormArray>this.signUpForm.get('hobbies')).controls;
+  }
+
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signUpForm.get('hobbies')).controls.push(control);
+  }
+}
