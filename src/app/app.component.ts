f26034da6bbf605f33726dbcab2b41810cc71f3b
mymafm0405m <mymafm0405@gmail.com>
2020-12-14 12:13:24 +0300

    Good reactive from with some custom sync validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,16 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signUpForm: FormGroup;
+  notAllowedUsername = ['mido', 'mahmoud'];
+  notEnoughLetters = false;
 
   ngOnInit() {
     this.signUpForm = new FormGroup({
-      'username': new FormControl(null, Validators.required),
+      'username': new FormControl(null, [Validators.required, this.checkAllowedNames.bind(this), this.countLetters.bind(this)]),
       'email': new FormControl(null, [Validators.required, Validators.email]),
       'gender': new FormControl('male'),
-      'hobbies': new FormArray([])
+      'hobbies': new FormArray([]),
+      'favourites': new FormArray([])
     })
   }
 
@@ -29,6 +32,43 @@ export class AppComponent implements OnInit {
 
   onAddHobby() {
     const control = new FormControl(null, Validators.required);
-    (<FormArray>this.signUpForm.get('hobbies')).controls.push(control);
+    (<FormArray>this.signUpForm.get('hobbies')).push(control);
+  }
+
+  getfavourites() {
+    return (<FormArray>this.signUpForm.get('favourites')).controls;
+  }
+
+  onAddFavouriet() {
+    const newControl = new FormControl(null, Validators.required);
+    (<FormArray>this.signUpForm.get('favourites')).push(newControl);
+  }
+
+  onRemoveHobby(index: number) {
+    (<FormArray>this.signUpForm.get('hobbies')).controls.splice(index, 1);
+    console.log('removed hobby' + index);
+  }
+
+  onRemoveFavourite(index: number) {
+    (<FormArray>this.signUpForm.get('favourites')).controls.splice(index, 1);
+    console.log('removed favourite' + index);
+  }
+
+  checkAllowedNames(control: FormControl): {[s: string]: boolean} {
+    if (this.notAllowedUsername.indexOf(control.value) !== -1) {
+      return {'notAllowed': true};
+    }
+    return null;
+  }
+
+  countLetters(control: FormControl): {[s: string]: boolean} {
+    if (control.value) {
+      if (control.value.length < 6 && this.notAllowedUsername.indexOf(control.value)) {
+        this.notEnoughLetters = true;
+        return {'notEnough': true};
+      }
+    }
+    this.notEnoughLetters = false;
+    return null;
   }
 }
